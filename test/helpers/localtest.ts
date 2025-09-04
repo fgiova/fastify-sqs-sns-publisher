186e8ca02042e7b4d6cdda6f5d39d9fda37be4f2
Francesco Giovannini <francesco.giovannini@coverzen.it>
2025-09-04 12:00:01 +0200

    feat!: update dependencies
    - Linting with biomejs
    - Improve Types
    - Esm/Cjs build
    
    BREAKING-CHANGE: Only for fastify 5.x

--- test/helpers/localtest.ts
+++ test/helpers/localtest.ts
@@ -1,18 +1,19 @@
-// @ts-ignore
-import path from "path"
-// @ts-ignore
-import fs from "fs";
-import {Socket} from "net";
-import {teardown} from "tap";
+import fs from "node:fs";
+import { Socket } from "node:net";
+import path from "node:path";
+import tap from "tap";
 
 const defaultExport = () => {
 	require("dotenv").config({
-		path: ".env.dev"
-	})
-	if(!process.env.TEST_LOCAL) {
-		const jsonString = fs.readFileSync(path.resolve(process.cwd(), "test-env.json"), {
-			encoding: "utf8"
-		});
+		path: ".env.dev",
+	});
+	if (!process.env.TEST_LOCAL) {
+		const jsonString = fs.readFileSync(
+			path.resolve(process.cwd(), "test-env.json"),
+			{
+				encoding: "utf8",
+			},
+		);
 		try {
 			const envConfig = JSON.parse(jsonString);
 
@@ -23,24 +24,27 @@ const defaultExport = () => {
 			console.error(err);
 		}
 	}
-	if(process.env.REAPER) {
+	if (process.env.REAPER) {
 		const [host, port] = process.env.REAPER.split(":");
 		const socket = new Socket();
 		socket.connect(Number(port), host, () => {
-			socket.write(`label=org.testcontainers.session-id=${process.env.REAPER_SESSION}\r\n`);
+			socket.write(
+				`label=org.testcontainers.session-id=${process.env.REAPER_SESSION}\r\n`,
+			);
 		});
 		socket.on("error", (error) => {
 			console.log(error);
 		});
 
-		teardown(() => {
-			socket.write("stop\r\n");
-			socket.destroy();
-			// force kill the process if it doesn't stop on its own
+		tap.teardown(() => {
 			setTimeout(() => {
-				process.exit(0);
+				socket.destroy();
+				// force kill the process if it doesn't stop on its own
+				setTimeout(() => {
+					process.exit(0);
+				}, 300);
 			}, 300);
 		});
 	}
-}
+};
 defaultExport();
--- test/unit/sns.ts
+++ test/unit/sns.ts
@@ -1,213 +1,225 @@
-import {test} from "tap";
+import { test } from "tap";
+// biome-ignore lint/suspicious/noTsIgnore: is a test file
 // @ts-ignore
 import "../helpers/localtest";
+import { setTimeout } from "node:timers/promises";
+import { SQS } from "@aws-sdk/client-sqs";
+import { MiniSQSClient } from "@fgiova/mini-sqs-client";
 import Fastify from "fastify";
-import publisherPlugin, {PublisherMessage} from "../../src";
-import { setTimeout } from "timers/promises";
-// @ts-ignore
-import {sqsPurge} from "../helpers/sqsMessage";
-import {MiniSQSClient} from "@fgiova/mini-sqs-client";
-import {SQS} from "@aws-sdk/client-sqs";
+import publisherPlugin, { PublisherMessage } from "../../src";
 
 const queueARN = "arn:aws:sqs:eu-central-1:000000000000:test-queue-topic";
 const topicARN = "arn:aws:sns:eu-central-1:000000000000:test-topic";
 
-test("sns publisher", async t => {
+test("sns publisher", async (t) => {
 	const awsSQS = new SQS({
-		endpoint: `${process.env.LOCALSTACK_ENDPOINT}`
+		endpoint: `${process.env.LOCALSTACK_ENDPOINT}`,
 	});
-	const sqs = new MiniSQSClient("eu-central-1", process.env.LOCALSTACK_ENDPOINT);
+	const sqs = new MiniSQSClient(
+		"eu-central-1",
+		process.env.LOCALSTACK_ENDPOINT,
+	);
 	t.teardown(async () => {
 		await sqs.destroy();
 	});
-	await t.test("SNS publish message", async t => {
+	await t.test("SNS publish message", async (t) => {
 		const app = Fastify();
 		t.teardown(async () => {
 			await app.close();
 		});
-		await app.register(publisherPlugin,  {
+		await app.register(publisherPlugin, {
 			sqs: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
 			},
 			sns: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
-			}
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
+			},
 		});
 		await app.ready();
-		await app.messageToSNS({message:"test-sns"}, topicARN);
+		await app.messageToSNS({ message: "test-sns" }, topicARN);
 		await setTimeout(1000);
 		const messages = await sqs.receiveMessage(queueARN, {});
-		const message = messages.Messages[0];
-		t.same(message.Body, "{\"message\":\"test-sns\"}");
-		await sqs.deleteMessage(queueARN, message.ReceiptHandle);
+		const message = messages.Messages?.[0];
+		t.same(message?.Body, '{"message":"test-sns"}');
+		// biome-ignore lint/style/noNonNullAssertion: ok if fails
+		await sqs.deleteMessage(queueARN, message?.ReceiptHandle!);
 	});
-	await t.test("SNS publish message as text", async t => {
+	await t.test("SNS publish message as text", async (t) => {
 		const app = Fastify();
 		t.teardown(async () => {
 			await app.close();
 		});
-		await app.register(publisherPlugin,  {
+		await app.register(publisherPlugin, {
 			sqs: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
 			},
 			sns: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
-			}
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
+			},
 		});
 		await app.ready();
-		await app.messageToSNS(JSON.stringify({message:"test-sns"}), topicARN);
+		await app.messageToSNS(JSON.stringify({ message: "test-sns" }), topicARN);
 		await setTimeout(1000);
 		const messages = await sqs.receiveMessage(queueARN, {});
-		const message = messages.Messages[0];
-		t.same(message.Body, "{\"message\":\"test-sns\"}");
-		await sqs.deleteMessage(queueARN, message.ReceiptHandle);
+		const message = messages.Messages?.[0];
+		t.same(message?.Body, '{"message":"test-sns"}');
+		// biome-ignore lint/style/noNonNullAssertion: ok if fails
+		await sqs.deleteMessage(queueARN, message?.ReceiptHandle!);
 	});
-	await t.test("SNS publish message w attributes", async t => {
+	await t.test("SNS publish message w attributes", async (t) => {
 		const app = Fastify();
 		t.teardown(async () => {
 			await app.close();
 		});
-		app.register(publisherPlugin,  {
+		app.register(publisherPlugin, {
 			sqs: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
 			},
 			sns: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
-			}
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
+			},
 		});
 		await app.ready();
-		const publisherMessage = new PublisherMessage({message:"test-sns"}, {
-			"test-attribute-number": 1,
-			"test-attribute-string": "test",
-			"test-attribute-array": ["test", "test2"],
-		});
+		const publisherMessage = new PublisherMessage(
+			{ message: "test-sns" },
+			{
+				"test-attribute-number": 1,
+				"test-attribute-string": "test",
+				"test-attribute-array": ["test", "test2"],
+			},
+		);
 		await app.messageToSNS(publisherMessage, topicARN);
 		await setTimeout(1000);
-		const messages = await sqs.receiveMessage(queueARN,{
+		const messages = await sqs.receiveMessage(queueARN, {
 			MessageAttributeNames: ["All"],
 		});
-		const message = messages.Messages[0];
-		t.same(message.Body, "{\"message\":\"test-sns\"}");
-		t.has(message.MessageAttributes["test-attribute-number"], {
-			"DataType": "Number",
-			"StringValue": "1"
+		const message = messages.Messages?.[0];
+		t.same(message?.Body, '{"message":"test-sns"}');
+		t.has(message?.MessageAttributes?.["test-attribute-number"], {
+			DataType: "Number",
+			StringValue: "1",
 		});
-		t.has(message.MessageAttributes["test-attribute-string"], {
-			"DataType": "String",
-			"StringValue": "test"
+		t.has(message?.MessageAttributes?.["test-attribute-string"], {
+			DataType: "String",
+			StringValue: "test",
 		});
-		t.has(message.MessageAttributes["test-attribute-array"], {
-			"DataType": "String.Array",
-			"StringValue": JSON.stringify(["test", "test2"])
+		t.has(message?.MessageAttributes?.["test-attribute-array"], {
+			DataType: "String.Array",
+			StringValue: JSON.stringify(["test", "test2"]),
 		});
-		await sqs.deleteMessage(queueARN, message.ReceiptHandle);
+		// biome-ignore lint/style/noNonNullAssertion: ok if fails
+		await sqs.deleteMessage(queueARN, message?.ReceiptHandle!);
 	});
-	await t.test("SNS publish message w attributes wrong data", async t => {
+	await t.test("SNS publish message w attributes wrong data", async (t) => {
 		const app = Fastify();
 		t.teardown(async () => {
 			await app.close();
 		});
-		app.register(publisherPlugin,  {
+		app.register(publisherPlugin, {
 			sqs: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
 			},
 			sns: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
-			}
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
+			},
 		});
 		await app.ready();
-		const publisherMessage = new PublisherMessage({message:"test-sns"}, {
+		const publisherMessage = new PublisherMessage({ message: "test-sns" }, {
 			"test-attribute-number": 1,
 			"test-attribute-string": "test",
 			"test-attribute-array": ["test", "test2"],
-			"test-attribute-wrong": {test: "test"}
+			"test-attribute-wrong": { test: "test" },
+			// biome-ignore lint/suspicious/noExplicitAny: it's a test
 		} as any);
 		try {
 			await app.messageToSNS(publisherMessage, topicARN);
-		}
-		catch (e) {
-			t.same(e.message, "Invalid data type");
+		} catch (e) {
+			t.same((e as Error).message, "Invalid data type");
 		}
 	});
 
-	await t.test("SNS publish message w attributes and filters", async t => {
+	await t.test("SNS publish message w attributes and filters", async (t) => {
 		const app = Fastify();
 		t.teardown(async () => {
 			await app.close();
 		});
-		await app.register(publisherPlugin,  {
+		await app.register(publisherPlugin, {
 			sqs: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
 			},
 			sns: {
 				region: "eu-central-1",
-				endpoint: process.env.LOCALSTACK_ENDPOINT
-			}
+				endpoint: process.env.LOCALSTACK_ENDPOINT,
+			},
 		});
 		await app.ready();
-		const publisherMessage = new PublisherMessage({message:"test-sns"}, {
-			"test-attribute-number": 1,
-			"test-attribute-string": "test",
-			"test-attribute-array": ["test", "test2"],
-		});
+		const publisherMessage = new PublisherMessage(
+			{ message: "test-sns" },
+			{
+				"test-attribute-number": 1,
+				"test-attribute-string": "test",
+				"test-attribute-array": ["test", "test2"],
+			},
+		);
 		await app.messageToSNS(publisherMessage, topicARN);
 		await setTimeout(1000);
-		const publisherMessage2 = new PublisherMessage({message:"test-sns-2"}, {
-			"test": "test",
-		});
+		const publisherMessage2 = new PublisherMessage(
+			{ message: "test-sns-2" },
+			{
+				test: "test",
+			},
+		);
 		await app.messageToSNS(publisherMessage2, topicARN);
 		await setTimeout(1000);
 		const queueAttributes = await awsSQS.getQueueAttributes({
 			QueueUrl: `${process.env.LOCALSTACK_ENDPOINT}/000000000000/test-queue-topic`,
-			AttributeNames: [
-				"All"
-			],
+			AttributeNames: ["All"],
 		});
-		t.equal(queueAttributes.Attributes.ApproximateNumberOfMessages, "2");
-		const messages = await sqs.receiveMessage(queueARN,{
+		t.equal(queueAttributes.Attributes?.ApproximateNumberOfMessages, "2");
+		const messages = await sqs.receiveMessage(queueARN, {
 			MessageAttributeNames: ["All"],
 		});
-		const message = messages.Messages[0];
-		t.same(message.Body, "{\"message\":\"test-sns\"}");
-		t.has(message.MessageAttributes["test-attribute-number"], {
-			"DataType": "Number",
-			"StringValue": "1"
+		const message = messages.Messages?.[0];
+		t.same(message?.Body, '{"message":"test-sns"}');
+		t.has(message?.MessageAttributes?.["test-attribute-number"], {
+			DataType: "Number",
+			StringValue: "1",
 		});
-		t.has(message.MessageAttributes["test-attribute-string"], {
-			"DataType": "String",
-			"StringValue": "test"
+		t.has(message?.MessageAttributes?.["test-attribute-string"], {
+			DataType: "String",
+			StringValue: "test",
 		});
-		t.has(message.MessageAttributes["test-attribute-array"], {
-			"DataType": "String.Array",
-			"StringValue": JSON.stringify(["test", "test2"])
+		t.has(message?.MessageAttributes?.["test-attribute-array"], {
+			DataType: "String.Array",
+			StringValue: JSON.stringify(["test", "test2"]),
 		});
 
 		const queueAttributes2 = await awsSQS.getQueueAttributes({
 			QueueUrl: `${process.env.LOCALSTACK_ENDPOINT}/000000000000/test-queue-topic-filtered`,
-			AttributeNames: [
-				"All"
-			],
+			AttributeNames: ["All"],
 		});
-		t.equal(queueAttributes2.Attributes.ApproximateNumberOfMessages, "1");
+		t.equal(queueAttributes2.Attributes?.ApproximateNumberOfMessages, "1");
 
-		const messages2 = await sqs.receiveMessage("arn:aws:sqs:eu-central-1:000000000000:test-queue-topic-filtered",{
-			MessageAttributeNames: ["All"]
-		});
-		const message2 = messages2.Messages[0];
-		t.same(message2.Body, "{\"message\":\"test-sns-2\"}");
-		t.has(message2.MessageAttributes.test, {
-			"DataType": "String",
-			"StringValue": "test"
+		const messages2 = await sqs.receiveMessage(
+			"arn:aws:sqs:eu-central-1:000000000000:test-queue-topic-filtered",
+			{
+				MessageAttributeNames: ["All"],
+			},
+		);
+		const message2 = messages2.Messages?.[0];
+		t.same(message2?.Body, '{"message":"test-sns-2"}');
+		t.has(message2?.MessageAttributes?.test, {
+			DataType: "String",
+			StringValue: "test",
 		});
 	});
 });
-
